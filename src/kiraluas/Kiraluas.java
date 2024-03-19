1c2093484a901515117698d889b7db91299ece3a
CikguMariana <163950954+CikguMariana@users.noreply.github.com>
2024-03-19 12:03:02 +0800

    Add files via upload

---
+++ src/kiraluas/Kiraluas.java
@@ -0,0 +1,19 @@
+/*
+input : tapak = 6, tinggi =4
+proses : formula luas = panjang * lebar
+output: papar luas segi empat
+ */
+package kiraluas;
+public class Kiraluas {
+    public static void main(String[] args) {
+        int panjang = 6;
+        int lebar = 4;
+        int luas;
+        
+        luas = panjang * lebar;
+        
+        System.out.println("Luas Segi Empat ialah " + luas);
+        
+    }
+    
+}
